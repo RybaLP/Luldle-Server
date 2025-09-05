786af8b03194823e7576f879d8142c9fd7a74e77
RybaLP <148885360+RybaLP@users.noreply.github.com>
2025-09-05 10:27:16 +0200

    Add Dockerfile and prepare for Render deployment

--- src/main/java/com/Luldle/Luldle/LuldleApplication.java
+++ src/main/java/com/Luldle/Luldle/LuldleApplication.java
@@ -8,13 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LuldleApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
 
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-		System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION", System.getenv("JWT_EXPIRATION"));
+		System.setProperty("DB_URL", System.getenv("DB_URL"));
+		System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+		System.setProperty("DB_USER", System.getenv("DB_USER"));
 
 		SpringApplication.run(LuldleApplication.class, args);
 	}
